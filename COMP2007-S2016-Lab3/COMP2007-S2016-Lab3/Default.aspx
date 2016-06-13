ea079d43435779be6bfab86fac74888fca2c78b2
FlyingArs <davidyu89tech@gmail.com>
2016-06-12 21:41:48 -0400

    Initial Setup + sticky footer

---
+++ COMP2007-S2016-Lab3/COMP2007-S2016-Lab3/Default.aspx
@@ -0,0 +1,6 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="COMP2007_S2016_Lab3.Default" %>
+
+<asp:Content ID="Content2" ContentPlaceHolderID="MainContent" runat="server">
+    <bs3:Jumbotron runat="server" id="Jumbotron">
+        </bs3:Jumbotron>
+</asp:Content>
---
+++ COMP2007-S2016-Lab3/COMP2007-S2016-Lab3/User_Controls/Navbar.ascx.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+/**
+ * @author: David Yu
+ * @date: May 27, 2016
+ * @version: 0.0.2 - updated the SetActivePage Method
+ */
+
+namespace COMP2007_S2016_Lab3
+{
+    public partial class Navbar : System.Web.UI.UserControl
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            SetActivePage();
+        }
+        /**
+         * This method adds a css class of "active" to list items related
+         * to navigation links of each page
+         *
+         * @method SetActivePage
+         * @return {void}
+         */
+
+        private void SetActivePage()
+        {
+            switch (Page.Title)
+            {
+                case "Home Page":
+                    home.Attributes.Add("class", "active");
+                    break;
+                case "Students":
+                    students.Attributes.Add("class", "active");
+                    break;
+                case "courses":
+                    courses.Attributes.Add("class", "active");
+                    break;
+                case "Departments":
+                    departments.Attributes.Add("class", "active");
+                    break;
+                case "Contact":
+                    contact.Attributes.Add("class", "active");
+                    break;
+            }
+        }
+    }
+}
